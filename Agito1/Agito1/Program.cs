845f5339fa73c0a35df39e227a780f9880fed09a
Hennadii Madan <madang@biph.kiev.ua>
2014-05-02 00:14:25 +0200

    Create repo, empty c# project, enter task description and some comments in source

---
+++ Agito1/Agito1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Task 1:
+ * 1.Create a Console Application project.
+ * 2.Please make your own ordinary text file. 
+ * 3.Words should be separated by spaces and newline characters. 
+ * 4.Your program must read the words from that text file into memory,
+ * 5.then sort those words (using QuickSort implementation of algorithm that can be found in .NET Base Class Library) 
+ * 6.and write the sorted words into a new text file onto the disk.
+ */
+
+/* Comments on the task:
+ * 
+ * 2. and 3. I assume from what is written in 3. that there should be no punctuation 
+ * just spaces,newlines and words from alphabetic characters.
+ * First I didn't assume that but then after re-reading the task this is how I interpret it.
+ */
+namespace Agito1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
